0fda586656bebbdd7b7e76b2c1f09777aabaf528
GraysonBerman <35932752+GraysonBerman@users.noreply.github.com>
2018-04-11 12:08:53 -0700

    Update College.java
    Made it an abstract class.
    Defined moves 1 and 2. Random works.

--- Project Mayhem/College.java
+++ Project Mayhem/College.java
@@ -1,21 +1,41 @@
-public class College {
-  private String name = "College";
-  private int health = 100;
-  private int damage;
+import java.util.Random;
+public abstract class College {
 
-  public College() {
-    System.out.println("constructed");
-  }
-  public int move1() {
-    //roll = 1 for demonstration
-    int roll = 1;
-    //roll = dice roll (random number between -3 and 3)
-    damage = 5 + roll;
+  //Strings each class needs
+  private String name;
+  private String major;
+  private int health;
+
+  //moves 1 and 2 are standard for all classes
+  //moves 3 and 4 are defined in derived classes
+  public static int move1(){
+    Random rand = new Random();
+    int baseDamage = 5;
+    int damage = baseDamage+rand.nextInt(10);
     return damage;
   }
-}
 
-class Dodge extends College {
-  //can make everything in player class or each college can be a different class.
+  public static int move2(){
+    Random rand = new Random();
+    int baseDamage = 5;
+    int damage = baseDamage + rand.nextInt(20);
+    return damage;
+  };
 
+  public abstract int move3();
+  public abstract int move4();
+  public static void main(String[] args){
+
+
+    //lines 31-34 compile correctly
+    int j = move1();
+    int k = move2();
+    System.out.println(j);
+    System.out.println(k);
+
+  }
+
+  //public College() {
+  //  System.out.println("constructed");
+  //}
 }
