3fbd9c5c27e973d2b6040c7a5f0dd998ffabbd7f
aslan104 <aslan104@mail.chapman.edu>
2018-04-24 15:50:25 -0700

    Create Client.java
    client attempt part 1

---
+++ Project Mayhem/Client.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.Socket;
+
+import java.util.Scanner;
+
+public class Client
+{
+  public static void main(String[] args)
+  {
+    try
+    {
+      String hostname = "localhost";
+      int port = 7654;
+
+      System.out.println("Connecting to server on port " + port);
+      Socket connectionSock = new Socket(hostname, port);
+      DataOutputStream serverOutput = new DataOutputStream(connectionSock.getOutputStream());
+      System.out.println("Connection made.");
+      ClientListener listener = new ClientListener(connectionSock);
+      Thread theThread = new Thread(listener);
+      theThread.start();
+      Scanner keyboard = new Scanner(System.in);
+      while (true)
+      {
+        String data = keyboard.nextLine();
+        serverOutput.writeBytes(data + "\n");
+      }
+    }
+    catch (IOException e)
+    {
+      System.out.println(e.getMessage());
+    }
+  }
+}
