b5a432c50609ae8cd12a6203eeaa5aa49997498f
Bailey Anderson <ander412@mail.chapman.edu>
2018-04-26 16:30:06 -0700

    Framework for Handler

--- Project Mayhem/ClientHandler.java
+++ Project Mayhem/ClientHandler.java
@@ -1,3 +1,12 @@
+/**
+ * ClientHandler.java
+ *
+ * This class handles communication between the client
+ * and the server.  It runs in a separate thread but has a
+ * link to a common list of sockets to handle broadcast.
+ *
+ */
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,54 +18,120 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class ClientHandler implements Runnable
-{
+public class ClientHandler implements Runnable {
   private Socket connectionSock = null;
   private ArrayList<Socket> socketList;
 
-  ClientHandler(Socket sock, ArrayList<Socket> socketList)
-  {
-    this.connectionSock = sock;
+
+
+  Socket player1;
+  Socket player2;
+  private BufferedReader player1Input;
+  private DataOutputStream player1Output;
+  private BufferedReader player2Input;
+  private DataOutputStream player2Output;
+  int Health1 = 100;
+  int Health2 = 100;
+
+
+  public ClientHandler(ArrayList<Socket> socketList){
     this.socketList = socketList;
   }
 
-  //received input from a client
-  public void run()
-    {
-    try
-    {
-      System.out.println("Connection made with socket " + connectionSock);
-      BufferedReader clientInput = new BufferedReader(
-          new InputStreamReader(connectionSock.getInputStream()));
-      while (true)
-      {
-        // Get data sent from a client
-        String clientText = clientInput.readLine();
-        if (clientText != null)
-        {
-          System.out.println("Received: " + clientText);
-          for (Socket s : socketList)
-          {
-            if (s != connectionSock)
-            {
-              DataOutputStream clientOutput = new DataOutputStream(s.getOutputStream());
-              clientOutput.writeBytes(clientText + "\n");
-            }
-          }
-        }
-        else
-        {
-          System.out.println("Closing connection for socket " + connectionSock);
-          socketList.remove(connectionSock);
-          connectionSock.close();
-          break;
-        }
-      }
-    }
-    catch (Exception e)
-    {
-      System.out.println("Error: " + e.toString());
-      socketList.remove(connectionSock);
+  //method that creates player 1
+  //need to get click data from gui character select screen
+  //Player Player1 = new Player(Name, catchphrase, college)
+
+  //method that creates player 2
+  //need to get click data from gui character select screen
+  //Player Player2 = new Player(Name, catchphrase, college)
+
+
+  //each college will have a number corresponding to it (collegeClass)
+  private void move(int moveNum, int collegeClass){
+    switch (moveNum) {
+      case 1: //method with logic for move1
+        break;
+      case 2: // method with logic for move2
+        break;
+      case 3: switch (collegeClass)
+                case 1: // method for schmid move3
+                  break;
+                case 2: //method for copa move3
+                  break;
+                //etc etc repeat for move 4 as well
     }
   }
-}
+
+  //logic for moves
+
+  public void move1(){
+    
+  }
+
+  public void move2(){
+    
+  }
+
+  public void move3Schmid(){
+    
+  }
+
+  public void move4Schmid(){
+    
+  }
+
+  //etc etc for all moves
+
+  private void clientTurn(Socket client, BufferedReader clientIn, DataOutputStream clientOut) {
+    //method that determines what happens each turn (win/lose etc.)
+  }
+
+
+
+  public void run() {
+    try {
+      
+      player1 = socketList.get(0);
+      System.out.println("Player1 connected");
+
+      //Player1's user
+      try {
+        player1Input = new BufferedReader(
+                new InputStreamReader(player1.getInputStream()));
+        player1Output = new DataOutputStream(player1.getOutputStream());
+        playerXOutput.writeBytes("Welcome to Project Mayhem. you are the first to connect\n"
+                + "Waiting for other player to connect...\n");
+
+        //Player2's user
+        while (true) {
+          if (socketList.size() == 2) {
+            //Init second user and send beginning message
+            Socket player2 = socketList.get(1);
+
+            player2Input = new BufferedReader(
+                    new InputStreamReader(player2.getInputStream()));
+            player2Output = new DataOutputStream(player2.getOutputStream());
+            player2Output.writeBytes("Welcome to Project Mayhem. you are the second to connect\n");
+            System.out.println("Client 2 connected");
+            break;
+          }
+        }
+        //Play the game
+        System.out.println("*Beginning Game*");
+        while (true) {
+          if (!playing)
+            break;
+          clientTurn(player1, player1Input, player1Output);
+          if (!playing)
+            break;
+          clientTurn(player2, player2Input, player2Output);
+        }
+      } catch (Exception e) {
+
+      }
+    }
+    catch(Exception e){
+    }
+  }
+} // ClientHandler for MtServer.java
