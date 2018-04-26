0e7f272e077418b246bf56159983ab30ee8f5b00
Bailey Anderson <ander412@mail.chapman.edu>
2018-04-26 16:31:11 -0700

    Framework for Listener

--- Project Mayhem/ClientListener.java
+++ Project Mayhem/ClientListener.java
@@ -1,3 +1,11 @@
+/**
+ * ClientListener.java
+ *
+ * This class runs on the client end and just
+ * displays any text received from the server.
+ *
+ */
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,40 +16,56 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static javafx.scene.input.KeyCode.PLAY;
 
-public class ClientListener implements Runnable
-{
+
+public class ClientListener implements Runnable {
   private Socket connectionSock = null;
+  private Player player = null;
+  private boolean running;
 
-  ClientListener(Socket sock)
-  {
+  ClientListener(Socket sock, Player player) {
     this.connectionSock = sock;
+    this.player = player;
+    this.running = true;
   }
 
-  public void run()
-  {
-    try
-    {
+  /**
+   * Gets message from server and displays it to the user.
+   */
+  public void run() {
+    try {
       BufferedReader serverInput = new BufferedReader(
           new InputStreamReader(connectionSock.getInputStream()));
-      while (true)
-      {
+      while (running) {
+        // Get data sent from the server
         String serverText = serverInput.readLine();
-        if (serverInput != null)
-        {
-          System.out.println(serverText);
-        }
-        else
-        {
-          System.out.println("Closing connection for socket " + connectionSock);
-          connectionSock.close();
-          break;
+        if (serverInput != null) {
+          //System.out.println("CLIENT DEBUG: " + serverText);
+          parseResponse(serverText);
         }
       }
-    }
-    catch (Exception e)
-    {
+    } catch (Exception e) {
       System.out.println("Error: " + e.toString());
     }
   }
+
+  private void parseResponse(String serverResponse){
+
+    if(serverResponse.equals("PLAY")){  //Client turn
+      player.play();
+    }
+    if(serverResponse.equals("WIN")){  //You win
+      player.win();
+      running = false;
+    }
+    if(serverResponse.equals("LOSE")){  //You lose
+      player.lose();
+      running = false;
+    }
+    else
+      System.out.println(serverResponse);
+  }
+
+
 }
