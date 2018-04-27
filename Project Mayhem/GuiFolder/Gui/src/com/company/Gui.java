05a7a9759c04db5c677d70f7c3884377da94ecbb
GraysonBerman <35932752+GraysonBerman@users.noreply.github.com>
2018-04-27 03:37:18 -0400

    Added gui files

---
+++ Project Mayhem/GuiFolder/Gui/src/com/company/Gui.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.awt.Frame;
+import java.awt.Panel;
+
+
+public class Gui extends Frame {
+
+    //constructor to set up gui components
+    public void guiprogram() {
+
+    };
+
+
+    Frame mainFrame = new Frame();
+    Panel pnl = new Panel();
+
+}
