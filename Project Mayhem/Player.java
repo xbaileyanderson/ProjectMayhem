0e88d09e44d9a3c1c53e93675821081f07448177
Bailey Anderson <ander412@mail.chapman.edu>
2018-05-08 13:37:01 -0700

    CoMpIleS aNd rUnS

--- Project Mayhem/Player.java
+++ Project Mayhem/Player.java
@@ -1,42 +1,128 @@
-public class Player {
-  private String college;
-  private String name;
-  private String catchphrase;
+/**
+ * MTClient.java
+ *
+ * This program implements a simple multithreaded chat client.  It connects to the
+ * server (assumed to be localhost on port 7654) and starts two threads:
+ * one for listening for data sent from the server, and another that waits
+ * for the user to type something in that will be sent to the server.
+ * Anything sent to the server is broadcast to all clients.
+ *
+ * The MTClient uses a ClientListener whose code is in a separate file.
+ * The ClientListener runs in a separate thread, recieves messages form the server,
+ * and displays them on the screen.
+ *
+ * Data received is sent to the output screen, so it is possible that as
+ * a user is typing in information a message from the server will be
+ * inserted.
+ *
+ */
 
-  public void setCollege(String c)
-  {
-    this.college = c;
+import java.io.*;
+
+import java.net.Socket;
+
+import java.util.Scanner;
+
+public class Player {
+  public static void main(String[] args) {
+    Player p = new Player();
+    p.startListener();
   }
-  public String getCollege()
-  {
-    return this.college;
+
+  private String hostname;
+  private int port;
+
+  private DataOutputStream serverOutput;
+  private Thread listenerThread;
+  private Scanner keyboard;
+
+
+  public Player(){
+    hostname = "localhost";
+    port = 7654;
+
+    keyboard = new Scanner(System.in);
+    initNetworking();
   }
-  public void setName(String n)
-  {
-    this.name = n;
+
+  private void initNetworking(){
+    try{
+      //Connect & Create
+      System.out.println("Creating connection objects...");
+
+      Socket connectionSock = new Socket(hostname, port);
+
+      serverOutput = new DataOutputStream(connectionSock.getOutputStream());
+      listenerThread = new Thread(new ClientListener(connectionSock, this));
+
+      System.out.println("*Client listener created*");
+    }
+    catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
   }
-  public String getName()
-  {
-    return this.name;
+
+  public void startListener(){
+    System.out.println("*Starting thread*");
+    listenerThread.start();
+
+    System.out.println("Connecting to server on port " + port + "...");
+    Thread.yield();
+    }
+
+
+
+
+  public void play(){
+
+    System.out.println("Make your move");
+      try{
+        String data = keyboard.nextLine();
+        ////gui input insted of keyboard input
+        serverOutput.writeBytes(data + "\n");
+      }
+      catch(Exception e){
+        System.out.println(e.getMessage());
+      }
   }
-  public void setCatchphrase(String cp)
-  {
-    this.catchphrase = cp;
+
+  //TODO: Add close methods
+  public void success(){
+    System.out.println("*Your was response was received by the server | your move was legitimate*");
+    System.out.println("Now waiting for your opponents response...");
   }
-  public String getCatchphrase()
-  {
-    return this.catchphrase;
+
+  public void failure(){
+    /*System.out.println("*Your response was received by the sever | your move was NOT legitimate*");
+    System.out.println("PLEASE ENTER A LEGAL NUMBER BETWEEN 1-8");
+    play(); */
   }
-  public int win()
-  {
-    return 1;
+
+  public void finished(){
+    System.out.println("*The other user disconnected, therefore you WIN (by default)*");
   }
-  public int lose()
-  {
-    return 1;
+
+  public void win(){
+    System.out.println("YOU WON!");
   }
-  public int play()
-  {
-    return 1;
+
+  public void lose(){
+    System.out.println("*YOU LOST!*");
+    System.out.println();
+  }
+
+  public void close(){
+    try {
+      System.out.println("Sending farewell msg to server...");
+      serverOutput.write("GOODBYE".getBytes());
+
+      listenerThread.join();
+      System.out.println("*Thread Closed*");
+
+      System.out.println("GOODBYE");
+    }
+    catch(Exception e){
+      System.out.println(e.getMessage());
+    }
   }
 }
