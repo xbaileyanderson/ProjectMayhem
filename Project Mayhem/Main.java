ff8eb5727579506e573ef58fd28e880e68d20e34
aslan104 <aslan104@mail.chapman.edu>
2018-05-10 17:03:21 -0700

    Delete Main.java

--- Project Mayhem/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    Player player1 = new Player();
-    player1.setName("Bob");
-    player1.setCollege("Dodge");
-    player1.setCatchphrase("Suh dude");
-    System.out.println(player1.getName());
-  }
-}
