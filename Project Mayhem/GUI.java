37a8e4d1b3a69f60fa2733a761588babcb143a22
GraysonBerman <35932752+GraysonBerman@users.noreply.github.com>
2018-05-10 17:02:03 -0700

    Updated GUI.Java
    Compiles in Terminal. Move 3 and Move 4 now change their visible text and terminal output when different classes are selected.

--- Project Mayhem/GUI.java
+++ Project Mayhem/GUI.java
@@ -1,3 +1,5 @@
+//This is the file that contains the games GUI
+//It is broken down into several components
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -10,7 +12,6 @@ import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTextField;
-import javax.swing.JList;
 import javax.swing.BoxLayout;
 import javax.swing.JTextArea;
 import javax.swing.JFormattedTextField;
@@ -20,18 +21,27 @@ import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import common.Player;
 import javax.swing.JTextPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GUI extends JFrame {
 
-	private JPanel contentPane;
-	private JPanel panel;
-	private JLabel lblSelectYourCollege;
-	private JList list;
-	private JLabel lblServerMessages;
-	private JPanel panel_1;
-	private JButton btnMove;
+	private JPanel contentPane; //contains everything
+	private JPanel panel; //top left panel, contains two labels, a text field, and a list (Name and College)
+	private JLabel lblSelectYourCollege;//Label before the next list
+	private JLabel lblServerMessages;//label above window where terminal messages are printed
+	private JPanel panel_1; //top right panel, contains 4 move buttons
+	private JButton btnMove_1;
+	private JButton btnArgyros;
+	private JButton btnCrean;
+	private JButton btnCOPA;
+	private JButton btnDodge;
+	private JButton btnSchmid;
+	private String tempMove3;
+	private String tempMove4;
+	private String move3Out = "test3";
+	private String move4Out = "test 4";
 
 	/**
 	 * Launch the application.
@@ -45,7 +55,7 @@ public class GUI extends JFrame {
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
 		});
 	}
@@ -60,56 +70,136 @@ public class GUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		panel = new JPanel();
-		panel.setBounds(6, 6, 211, 112);
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		panel.setBounds(6, 6, 211, 124);
 		contentPane.add(panel);
-		
-		lblSelectYourCollege = new JLabel("Select your College");
-		panel.add(lblSelectYourCollege);
-		
-		list = new JList();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Argyros", "Crean", "College of the Arts", "Dodge", "Schmid"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		panel.add(list);
-		
+
+
+
 		lblServerMessages = new JLabel("Server Messages");
 		lblServerMessages.setBounds(16, 130, 111, 38);
 		contentPane.add(lblServerMessages);
-		
+
 		panel_1 = new JPanel();
 		panel_1.setBounds(240, 6, 204, 137);
 		contentPane.add(panel_1);
-		
-		btnMove = new JButton("Move 1");
-		btnMove.addMouseListener(new MouseAdapter() {
+
+		btnMove_1 = new JButton("Basic Attack 1");//move1
+		btnMove_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("1");
 			}
 		});
-		panel_1.add(btnMove);
-		
-		JButton btnMove_1 = new JButton("Move 2");
-		btnMove_1.addMouseListener(new MouseAdapter() {
+		panel_1.add(btnMove_1);//move 1
+
+		JButton btnMove_2 = new JButton("Basic Attack 2");
+		btnMove_2.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				System.out.println("2");
 			}
 		});
-		panel_1.add(btnMove_1);
-		
+		panel_1.add(btnMove_2);//move2
+
+		String tempMove3 = "Move 3";
+		JButton btnMove_3 = new JButton(tempMove3);//move3
+		btnMove_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println(move3Out);
+			}
+		});
+		panel_1.add(btnMove_3);
+
+		String tempMove4 = "Move 4";
+		JButton btnMove_4 = new JButton(tempMove4);//move4
+		btnMove_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println(move4Out);
+			}
+		});
+		panel_1.add(btnMove_4);
+
 		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
 		textPane.setBounds(6, 160, 438, 112);
 
 		contentPane.add(textPane);
-		
+
+
+		//Here you select your college by clicking one of the college buttons in the top left side of the GUI.
+		//After clicking one of the college buttons, the move 3 and move 4 button on the right side of the gui
+		//changes name and output, based on the college you selected.
+		lblSelectYourCollege = new JLabel("Select your College");
+		panel.add(lblSelectYourCollege);
+
+		btnArgyros = new JButton("Argyros");
+		btnArgyros.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				btnMove_3.setText("Argyros Move 3");
+				btnMove_4.setText("Argyros Move 4");
+				move3Out = "move3Argyros";
+				move4Out = "move4Argyros";
+			}
+		});
+		panel.add(btnArgyros);
+
+		btnCOPA = new JButton("COPA");
+		btnCOPA.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+		btnCOPA.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnMove_3.setText("COPA Move 3");
+				btnMove_4.setText("COPA Move 4");
+				move3Out = "move3COPA";
+				move4Out = "move4COPA";
+			}
+		});
+		panel.add(btnCOPA);
+
+		btnCrean = new JButton("Crean");
+		btnCrean.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				btnMove_3.setText("Crean Move 3");
+				btnMove_4.setText("Crean Move 4");
+				move3Out = "move3Crean";
+				move4Out = "move4Crean";
+			}
+		});
+		panel.add(btnCrean);
+
+		btnDodge = new JButton("Dodge");
+		btnDodge.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				btnMove_3.setText("Dodge Move 3");
+				btnMove_4.setText("Dodge Move 4");
+				move3Out = "move3Dodge";
+				move4Out = "move4Dodge";
+			}
+		});
+		panel.add(btnDodge);
+
+		btnSchmid = new JButton("Schmid");
+		btnSchmid.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				btnMove_3.setText("Schmid Move 3");
+				btnMove_4.setText("Schmid Move 4");
+				move3Out = "move3Schmid";
+				move4Out = "move4Schmid";
+			}
+		});
+		panel.add(btnSchmid);
+
 	}
 }
