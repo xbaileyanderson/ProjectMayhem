85912f4ccc59119a00b209a03e14056a58d8951d
Bailey Anderson <ander412@mail.chapman.edu>
2018-05-11 02:44:45 -0700

    final

--- Project Mayhem/GameServer.java
+++ Project Mayhem/GameServer.java
@@ -1,18 +1,3 @@
-/**
- * MTServer.java
- *
- * This program implements a simple multithreaded chat server.  Every client that
- * connects to the server can broadcast data to all other clients.
- * The server stores an ArrayList of sockets to perform the broadcast.
- *
- * The MTServer uses a ClientHandler whose code is in a separate file.
- * When a client connects, the MTServer starts a ClientHandler in a separate thread
- * to receive messages from the client.
- *
- * To test, start the server first, then start multiple clients and type messages
- * in the client windows.
- *
- */
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -99,4 +84,4 @@ public class GameServer {
     GameServer server = new GameServer();
     server.getConnection();
   }
-} // MtServer
+}
